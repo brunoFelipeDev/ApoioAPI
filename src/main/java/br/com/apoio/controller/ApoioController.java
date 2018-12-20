afd641ae67f71c97283ef99d06a89958400840de
Bruno Felipe Mende de Sá Menezes <brunofelipe@syncode.local>
2018-12-20 12:34:21 -0200

    inicio

---
+++ src/main/java/br/com/apoio/controller/ApoioController.java
@@ -0,0 +1,15 @@
+package br.com.apoio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApoioController {
+
+	@GetMapping
+	@RequestMapping(value = "/cursos")
+	public String listar() {
+		return "deu certo";
+	}
+}
